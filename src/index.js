0e5e6e2e09aa084e293c712f3d8d4e39a1dcfab9
Tribhuwan <mathincosmos@gmail.com>
2022-07-31 09:02:52 +0530

    fix the DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import './style.css'
-
+import img from './img.jpeg'
+import github from './github.png'
 import homePage from '../src/home.js'
 import menuPage from '../src/menu.js'
 import contactPage from '../src/contact.js'
+
+// const gitImg = new Image();
+// gitImg.src = github;
+// gitImg.classList.add("github");
+// gitImg.alt = "github";
+// const homeImg = new Image();
+// homeImg.src = img;
+// homeImg.alt = "homeImg";
+
+
 document.body.innerHTML = `<div class="header">
         <div class="cont">
             <div class="name">Eaty</div>
@@ -21,7 +32,7 @@ document.body.innerHTML = `<div class="header">
     </div>
     <div class="footer">
         <div class="cite">Crafted by Tjsm</div>
-        <div class="img"> <a href="https://github.com/Tribhuwan-Joshi"></a> <img src="../src/github.png" alt="github"
+        <div class="img"> <a href="https://github.com/Tribhuwan-Joshi"></a> <img src=${github} alt="github"
                 class="github" title="Github"></div>
     </div>`;
   
@@ -30,7 +41,7 @@ const navButtons = document.querySelectorAll(".nav div");
 const main = document.querySelector(".main");
 main.innerHTML = `<div class="content home">
 <h1> 🔥 The Taste of Heaven 🔥</h1>
-<img src="../src/img.jpeg" alt="area">
+<img src=${img} alt="img">
 <span class="text"> <span class="first">E</span>aty is a imaginary restaurant created to learn webpacks in CSS.
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa quod omnis doloribus maiores voluptatibus
     repudiandae. Placeat doloribus ex quod, corporis eius quis accusantium vitae excepturi laboriosam, natus facere
